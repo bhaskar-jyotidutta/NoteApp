75ac59150ebcdce18b004e47ec7c10d9f8f30e85
Bhaskar_Jyoti_Dutta <bhaskarduttapersonal@gmail.com>
2023-07-03 00:27:30 +0530

    All component added, default three notes visible

--- app/src/main/java/com/example/architectureexample/MainActivity.java
+++ app/src/main/java/com/example/architectureexample/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviderGetKt;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -18,11 +21,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
+        /*instantiating ViewModel. We don't call new NoteViewModel() because it will then create a instance with every activity
+        * . Instead we have to ask Android because Android know when to create a new instance or when to give
+        * already created instance*/
+
+        /*With the keyword this the ViewModel knows with which activity is has scope. When this activity will
+        destroy then Android will destroy this ViewModel also*/
         noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+
+        /*Here observe() is LiveData method*/
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
-
+               adapter.setNotes(notes);
             }
         });
 
