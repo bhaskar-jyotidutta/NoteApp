110a2566eaaf80a04c86b6413efcfb9c5a8a8e1d
Bhaskar <bhaskarduttapersonal@gmail.com>
2023-07-02 19:34:41 +0530

    Code added in Repository class, NoteViewModel Class code

--- app/src/main/java/com/example/architectureexample/MainActivity.java
+++ app/src/main/java/com/example/architectureexample/MainActivity.java
@@ -1,16 +1,30 @@
 package com.example.architectureexample;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviderGetKt;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private NoteViewModel noteViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
+        noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(List<Note> notes) {
+
+            }
+        });
 
     }
 }
